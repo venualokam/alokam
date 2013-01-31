028de990576c3e2d0b77af801812af02af6e62ee
venualokam <venu.alokam@fadv.com>
2013-01-31 16:05:05 +0530

    xcvcxv

--- src/alokam/MTOMImpl.java
+++ src/alokam/MTOMImpl.java
@@ -11,6 +11,7 @@ import javax.xml.ws.soap.MTOM;
 public class MTOMImpl {
   @WebMethod
   public String echoBinaryAsString(byte[] bytes) {
+	  System.out.println("venu alokam --------");
     return new String(bytes);
 
   }
