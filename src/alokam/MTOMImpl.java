d91db6d7e7fa62a435fb59d68ed90ebe81f2bf99
venualokam <venualokam@gmail.com>
2015-01-12 17:21:28 +0530

    alokam chanages

--- src/alokam/MTOMImpl.java
+++ src/alokam/MTOMImpl.java
@@ -11,7 +11,7 @@ import javax.xml.ws.soap.MTOM;
 public class MTOMImpl {
   @WebMethod
   public String echoBinaryAsString(byte[] bytes) {
-	  System.out.println("venu alokam --ccc---vv-xczxc asd 444444 fff sdfsdfsdf--");
+	  System.out.println("venu alokam --ccc---vv-xczxc asd --");
     return new String(bytes);
 
   }
